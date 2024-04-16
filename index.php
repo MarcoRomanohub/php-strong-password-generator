966d5b904569066d485bdf5850b256ab41fea2ae
MarcoRomanoHub <marcoromano270@gmail.com>
2024-04-16 16:46:43 +0200

    [add] milestone 2

--- index.php
+++ index.php
@@ -1,20 +1,6 @@
 <?php
 $password_length = $_GET['lunghezza_password'];
-function generateRandomString($password_length)
-{
-  $password_length = $_GET['lunghezza_password'];
-  $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!?&%$<>^+-*/()[]{}@#_=';
-  $charactersLength = strlen($characters);
-  $randomString = '';
-  if (isset($_GET['lunghezza_password'])) {
-    for ($i = 0; $i < $password_length; $i++) {
-      $randomString .= $characters[random_int(0, $charactersLength - 1)];
-    }
-  } else {
-    $randomString = '';
-  }
-  return $randomString;
-}
+include_once __DIR__ . ('/data/functions.php')
 
 
 ?>
